fb5725484cb8e23790e71a04db125b34ca4f533f
Luis <lraulam.03@gmail.com>
2022-11-27 23:52:17 -0600

    Added addVertexFunction

--- DGraph.c
+++ DGraph.c
@@ -1,12 +1,14 @@
 #include "DGraph.h"
 #include "Contenedores/List.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct dgraph_struct {
     List vertex;
     List edge;
     int size;
     size_t elementSize;
+    size_t tagSize;
     fcmp cmpTag;
     fcmp cmpData;
 };
@@ -16,7 +18,7 @@ struct vertex_struct {
     Type data;
     Type tag;
 };
-typedef struct node_struct * Vertex;
+typedef struct vertex_struct * Vertex;
 
 struct edge_struct {
     Vertex source;
@@ -25,14 +27,46 @@ struct edge_struct {
 };
 typedef struct edge_struct * Edge;
 
-DGraph createDGraph(size_t elementSize, fcmp cmpTag, fcmp cmpData) {
+DGraph createDGraph(size_t elementSize, size_t tagSize, fcmp cmpTag, fcmp cmpData) {
     DGraph new = malloc(sizeof(struct dgraph_struct));
-    new->vertex = listCreate(sizeof(Vertex));
+    new->vertex = listCreate(sizeof(struct vertex_struct)); // testing
     new->edge = listCreate(sizeof(Edge));
     new->size = 0;
     new->elementSize = elementSize;
+    new->tagSize = tagSize;
     new->cmpTag = cmpTag;
     new->cmpData = cmpData;
 
     return new;
+}
+
+Bool vertexExists(DGraph DG, Type tag) {
+    Vertex current;
+
+    for (int i = 0; i < DG->size; i++) {
+        current = listGet(DG->vertex, i);
+        if (DG->cmpTag(tag, current->tag) == 0)
+            return TRUE;
+    }
+    return FALSE;
+}
+
+void addVertex(DGraph DG, Type data, Type tag) {
+    if (vertexExists(DG, tag)) return;
+
+    Vertex new = malloc(sizeof(struct vertex_struct));
+    if (!new) return;
+
+    new->neighbours = listCreate(sizeof(struct vertex_struct));
+
+    Type tempData = malloc(DG->elementSize);
+    memcpy(tempData, data, DG->elementSize);
+    new->data = tempData;
+
+    Type tempTag = malloc(DG->tagSize);
+    memcpy(tempTag, tag, DG->tagSize);
+    new->tag = tempTag;
+
+    listAdd(DG->vertex, new);
+    DG->size++;
 }
